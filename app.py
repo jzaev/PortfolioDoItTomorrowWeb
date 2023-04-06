2110026d81c07ca91bc9d6afbfad3a202281469d
Yuri Zaev <juryzaev@gmail.com>
2023-04-06 18:17:33 +0700

    delete and move buttons added and working.

--- app.py
+++ app.py
@@ -50,6 +50,23 @@ def toggle_task_completion(task_id):
 
     return redirect(url_for('index'))
 
+@app.route('/delete-task/<int:task_id>', methods=['POST'])
+def delete_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    db.session.delete(task)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/move-task/<int:task_id>', methods=['POST'])
+def move_task(task_id):
+    task = Task.query.get_or_404(task_id)
+    if task.due_date == date.today():
+        task.due_date = date.today() + timedelta(days=1)
+    else:
+        task.due_date = date.today()
+    db.session.commit()
+    return redirect(url_for('index'))
+
 
 class Task(db.Model):
     id = Column(Integer, primary_key=True)
